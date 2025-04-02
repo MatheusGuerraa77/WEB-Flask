a5448c23179ad41ffc58c497550aa9fc1f3557eb
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2025-04-01 23:44:41 -0300

    Valores e Variáveis

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 # localhost:5000/
 @app.route('/')
 def principal():
-    return render_template("index.html")
+    conteudo1 = 'Manipulação de Dados'
+    conteudo2 = 'Herança e Templates'
+    conteudo3 = 'Integração de APIs'
+    conteudo4 = 'Banco de Dados'
+    return render_template(
+        "index.html",
+        conteudo1=conteudo1,
+        conteudo2=conteudo2,
+        conteudo3=conteudo3,
+        conteudo4=conteudo4
+    )
 
 @app.route('/sobre')
 def sobre():
